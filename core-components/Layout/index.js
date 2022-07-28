276109c09b823734400fa7f86d5c6d9b27b08b3c
Jiří Zmrhal <jiri.zmrhal@actumdigital.com>
2022-07-28 11:00:25 +0200

    Conditionally render main landmark

--- core-components/Layout/index.js
+++ core-components/Layout/index.js
@@ -6,18 +6,28 @@ export default function Layout({ children }) {
   const router = useRouter();
   const {asPath} = router
 
+  const renderContent = (children) => (
+    <div className="container">
+      { asPath !== '/' && <Header /> }
+      {children}
+    </div>
+  )
+
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="ACTUM Collection of inaccessible components used for education purposes" />
       </Head>
-      <main>
-        <div className="container">
-          { asPath !== '/' && <Header /> }
-          {children}
-        </div>
-      </main>
+      {
+        // Do not render the main landmark on the accordion page
+        // To demonstrate the accordion issue "Usage of the header element is invalid in this case."
+        asPath !== '/page-accordion' ? (
+          <main>
+            {renderContent(children)}
+          </main>
+        ) : renderContent(children)
+      }
     </>
   )
 }
